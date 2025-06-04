9c31f00c1737784da19cc364b1a336cfe5767452
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-04 14:56:06 +0530

    complete the user deshboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../other/header";
 import TaskListNumbers from "../other/TaskListNumbers";
+import TaskList from "../Task/TaskList";
+
 
 const EmployeeDashboard = ()=>{
     return (
-    <div className="p-10 bg-[#1C1C1C] h-screen">
+    <div className="p-10 bg-[#1C1C1C] w-screen h-screen">
         <Header />
         <TaskListNumbers />
+        <TaskList />
     </div>
 );
 }
---
+++ src/components/Task/TaskList.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full mt-10  py-5"
+    >
+      <div className="flex-shrink-0 h-full w-[300px] bg-r rounded-xl bg-blue-500 p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm py-1 px-3 rounded-md text-white">
+            high
+          </h3>
+          <h4 className="text-sm">4 June 2025</h4>
+        </div>
+        <h2 className="text-2xl font-bold mt-5">Do a mern project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          dignissimos fugiat adipisci. Iste quibusdam voluptates delectus
+          doloribus ut dolorem quasi?.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-r bg-lime-600 rounded-xl p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm py-1 px-3 rounded-md text-white">
+            high
+          </h3>
+          <h4 className="text-sm">4 June 2025</h4>
+        </div>
+        <h2 className="text-2xl font-bold mt-5">Do a mern project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          dignissimos fugiat adipisci. Iste quibusdam voluptates delectus
+          doloribus ut dolorem quasi?.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-500 bg-r rounded-xl p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm py-1 px-3 rounded-md text-white">
+            high
+          </h3>
+          <h4 className="text-sm">4 June 2025</h4>
+        </div>
+
+        <h2 className="text-2xl font-bold mt-5 text-black">Do a mern project</h2>
+        <p className="text-sm mt-2 text-black">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          dignissimos fugiat adipisci. Iste quibusdam voluptates delectus
+          doloribus ut dolorem quasi?.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-orange-500 bg-r rounded-xl p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm py-1 px-3 rounded-md text-white">
+            high
+          </h3>
+          <h4 className="text-sm">4 June 2025</h4>
+        </div>
+
+        <h2 className="text-2xl font-bold mt-5">Do a mern project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          dignissimos fugiat adipisci. Iste quibusdam voluptates delectus
+          doloribus ut dolorem quasi?.
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-blue-500 bg-r rounded-xl p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm py-1 px-3 rounded-md text-white">
+            high
+          </h3>
+          <h4 className="text-sm">4 June 2025</h4>
+        </div>
+
+        <h2 className="text-2xl font-bold mt-5">Do a mern project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          dignissimos fugiat adipisci. Iste quibusdam voluptates delectus
+          doloribus ut dolorem quasi?.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
