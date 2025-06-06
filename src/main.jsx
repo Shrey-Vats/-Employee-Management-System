bce747f74667cb03b6889c039ab17d548e1c4d25
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-06 22:06:00 +0530

    update logic in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import your context providers
+import { AuthContextProvider } from "./context/AuthContext";
+import { TaskContextProvider } from "./context/TaskContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* AuthContextProvider should be higher in the tree if TaskContextProvider depends on it */}
+    <AuthContextProvider>
+      <TaskContextProvider>
+        <App />
+      </TaskContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
